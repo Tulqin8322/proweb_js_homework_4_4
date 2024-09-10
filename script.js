3790d8705fefc7bf0ba44c3893af2ed98c721a96
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2024-09-10 15:15:46 +0500

    3 tasi tayyor

---
+++ script.js
@@ -0,0 +1,46 @@
+    
+while (true) {
+var son_t_j=+prompt("son kiriting");
+    while(isNaN(son_t_j)||son_t_j==""){
+    son_t_j=+prompt("Iltimos son kiriting");
+} 
+if(son_t_j%2==1){
+    alert(son_t_j+" toq son")
+} else{
+    alert(son_t_j+" juft son")
+}
+}
+
+var son_daraja=1;
+var son=+prompt("Darajaga oshiriladigan sonni kiriting")
+while(isNaN(son)||son==""){
+    son=+prompt("Iltimos darajaga oshiriladigan sonni kiriting");
+} 
+var daraja=+prompt("Sonning darajasini kiriting");
+while(isNaN(daraja)||daraja==""){
+    daraja=+prompt("Iltimos sonning darajasini kiriting");
+} 
+for(let i=0; i<daraja; i++){
+    son_daraja=son_daraja*son
+}
+alert(son+" ning "+daraja+" si "+son_daraja+" ga teng")
+
+
+var a;
+var chessBord="";
+a=+prompt("son kiriting")
+while(isNaN(a)||a==""){
+    a=+prompt("Iltimos son kiriting")
+}
+for(let i=0; i<a; i++){
+    for(let j=0; j<a; j++){
+        if(i==0||j==0||i==j|i==a-1||j==a-1){
+            chessBord+="⬛";
+        } else {
+            chessBord+="⬜";
+        }    
+    }
+    console.log(chessBord);
+    chessBord="";
+}
+
